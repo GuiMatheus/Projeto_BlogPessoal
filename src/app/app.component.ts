f975f78f2cea9999124a3cff333aa057fd5f7e93
Gui Matheus <guilherme.maths.sil@gmail.com>
2021-09-02 23:32:50 -0300

    Componente inicio funcional sem consumo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Ola Guih';
+
+  constructor(
+    public auth: AuthService
+  ) {}
+
+ 
+  
+
+
+
 }
+
